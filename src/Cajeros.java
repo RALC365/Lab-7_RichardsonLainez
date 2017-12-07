71d4efc8eb8cf5df8f632f30d361b1bbdc8ee57a
RALC365 <alejandro.carcamo@unitec.edu>
2017-12-07 12:39:49 -0600

    Interfaz creada

---
+++ src/Cajeros.java
@@ -0,0 +1,19 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RALC
+ */
+public class Cajeros {
+    private String nombre;
+    private int id;
+    
+   ArrayList<Ordenes> ordenes = new ArrayList();
+}
